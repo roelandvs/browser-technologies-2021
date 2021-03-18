b74de78f3349a0c48f8a7f1009907d59d49066d9
Roeland <roelandsteevan@gmail.com>
2021-03-18 12:16:09 +0100

    express working

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// const { dataTransformation } = require('./js/data/dataTransformation');
+// const { detailDataTransformation } = require('./js/data/detailDataTransformation');
+
+app
+    .use(express.static('public'))
+    .set('view engine', 'ejs')
+
+//routes
+app.get('/', (req, res) => {
+    // dataTransformation()
+    //     .then(response => {
+            res.render('pages/overview')
+        // });
+})
+
+// app.get('/:id', (req, res) => {
+//     detailDataTransformation(req.params.id)
+//         .then(response => {
+//             res.render('pages/detail', { data: response })
+//         });
+// })
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
