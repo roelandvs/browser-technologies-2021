b3589afc6054ca07bdaa98c66379577835051960
Roeland <roelandsteevan@gmail.com>
2021-03-27 13:40:40 +0100

    localStorage working

--- public/js/formSteps.js
+++
@@ -1,8 +0,0 @@
-const form = document.querySelector('form');
-
-form.addEventListener('focusout', function (event) {
-    console.log(event.target.value);
-    let testObject = { 'one': 1, 'two': 2, 'three': 3 };
-
-    localStorage.setItem('testObject', JSON.stringify(testObject))
-}, true);
--- public/js/localStorage.js
+++ public/js/localStorage.js
@@ -1,5 +1,24 @@
-function saveInputs() {
-    const fieldsets = window.getElement
+const form = document.querySelector('form');
+const inputs = document.querySelectorAll('form input');
+
+function addInputs() {
+    inputs.forEach(input => {
+        const storedInput = localStorage.getItem(`${form.id}-${input.name}`);
+
+        if(storedInput) {
+            if(input.type === 'radio' && input.value === storedInput) {
+                input.checked = true;
+                // console.log(input.value)
+            } else if(input.type !== 'radio') {
+                input.value = storedInput;
+            };
+        };
+    });
 };
 
-saveInputs();
+//maybe event focusout is better for performance?
+form.addEventListener('focusout', function (event) {
+    localStorage.setItem(`${form.id}-${event.target.name}`, event.target.value)
+}, true);
+
+window.addEventListener('load', addInputs)
