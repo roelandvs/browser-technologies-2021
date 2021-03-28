82fb54d2b48006e7efa1fe52374b2d475a31837d
Roeland <roelandsteevan@gmail.com>
2021-03-28 19:38:09 +0200

    form check complete

--- public/js/checkFieldset.js
+++ public/js/checkFieldset.js
@@ -1,11 +1,18 @@
 export function checkFieldset(fieldsets, activeFieldset) {
     const input = fieldsets[activeFieldset].getElementsByTagName('input');
+
+    let radioCheck = {
+        'algemeen-beoordeling': false,
+        'moeilijkheidsgraad': false,
+        'kwaliteit': false,
+    };
+
     let valid = true;
-    // console.log('gaat door funtcie');
 
     for (let i = 0; i < input.length; i++) {
-        switch (input[i].getAttribute("name")) {
-            case "docent":
+        // console.log(input[i].getAttribute("name"))
+        switch (input[i].getAttribute('name')) {
+            case 'docent':
                 if (input[i].value.length < 2) {
                     input[i].classList.add('invalid');
                     valid = false;
@@ -13,9 +20,9 @@ export function checkFieldset(fieldsets, activeFieldset) {
                     input[i].classList.remove('invalid');
                     // document.getElementById('error-name').innerHTML = '';
                 }
-                break;
+            break;
 
-            case "name":
+            case 'name':
                 if (input[i].value.length < 2) {
                     input[i].classList.add('invalid');
                     valid = false;
@@ -23,7 +30,37 @@ export function checkFieldset(fieldsets, activeFieldset) {
                     input[i].classList.remove('invalid');
                     // document.getElementById('error-name').innerHTML = '';
                 }
-                break;
+            break;
+
+            case 'begin':
+            case 'einde':
+                if (new Date(input[i].value) == 'Invalid Date') {
+                    input[i].classList.add('invalid');
+                    valid = false;
+                } else if (input[i].classList.contains('invalid')) {
+                    input[i].classList.remove('invalid');
+                    // document.getElementById('error-name').innerHTML = '';
+                }
+            break;
+
+            case 'algemeen-beoordeling':
+            case 'moeilijkheidsgraad':
+            case 'kwaliteit':
+                const parentContainer = document.getElementById(input[i].getAttribute('name'));
+
+                if (input[i].checked === true) {
+                    radioCheck[input[i].getAttribute('name')] = true;
+                }
+
+                if (radioCheck[input[i].getAttribute('name')] === false) {
+                    parentContainer.classList.add('invalid');
+                    valid = false;
+                } else if (parentContainer.classList.contains('invalid')) {
+                    parentContainer.classList.remove('invalid');
+                    // document.getElementById('error-name').innerHTML = '';
+                    valid = true;
+                }
+            break;
         };
     };
 
